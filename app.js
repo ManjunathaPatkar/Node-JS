95d79010f447681a6aaca96718ba8582eb0978e3
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2020-12-30 16:26:29 +0530

    File system module demonstration

--- app.js
+++ app.js
@@ -15,11 +15,31 @@
 // const path=require('path');
 // var pathobj=path.parse(__filename);
 // console.log(pathobj);
-const os=require('os');
-var totalmem=os.totalmem();
-var freemme=os.freemem();
-// console.log(totalmem);
-// console.log(freemme);
-//using template string to print
-console.log(`total memeory ${totalmem}`);
-console.log(`free memory ${freemme}`);
+// const os=require('os');
+// var totalmem=os.totalmem();
+// var freemme=os.freemem();
+// // console.log(totalmem);
+// // console.log(freemme);
+// //using template string to print
+// console.log(`total memeory ${totalmem}`);
+// console.log(`free memory ${freemme}`);
+const fs=require('fs');
+const fils=fs.readdirSync('./');
+// console.log(fils);
+console.log(`${fils}`);
+fs.readdir('./',function (err,result){
+ if(err){
+     console.log(err);
+ }
+ else{
+     console.log(result);
+ }
+
+})
+fs.readdir('./wffasdf',function (err,result) {//with error
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(result);
+    }
+})
