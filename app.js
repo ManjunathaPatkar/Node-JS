938c269d198a035b64e57e2fb0fb7c4ef3a39e5d
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2020-12-30 14:40:50 +0530

    Var is not global in node so if we run this we get undefined

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-function sayhello(name) {
-console.log("Hello "+name);
-}
-// sayhello("Manja");
-// sayhello("Patkar");
-// sayhello("Macbook");
-// sayhello("Air");
-console.log(window);
+// function sayhello(name) {
+// console.log("Hello "+name);
+// }
+// // sayhello("Manja");
+// // sayhello("Patkar");
+// // sayhello("Macbook");
+// // sayhello("Air");
+// console.log(window);
+var a="manja";
+console.log(global.message);//variabes are not global
