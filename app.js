ad3a07e159691c87eb26a3cb679463e4ade6d6a7
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2020-12-30 15:15:28 +0530

    exporting functions from one model and importing it in other model so we can use functions in other module in imported module

--- app.js
+++ app.js
@@ -8,4 +8,6 @@
 // console.log(window);
 // var a="manja";
 // console.log(global.message);//variabes are not global so we get undefined as output
-console.log(module);
+// console.log(module);
+var logger=require('./logger.js');
+console.log(logger);
---
+++ logger.js
@@ -0,0 +1,7 @@
+var url='https://somewebsite name.com/log';
+function log(message) {
+    //we will send HTTP request here
+    console.log(message);
+}
+// module.exports.URL=url;
+module.exports.log=log;
